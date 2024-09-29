d4d9ef46dec8a602bf5b903c211d5923cde13226
jova326 <113640799+jova326@users.noreply.github.com>
2024-09-29 09:37:26 -0500

    Add files via upload

--- Capstone_Student_Evaluator/calculation.js
+++ Capstone_Student_Evaluator/calculation.js
@@ -12,17 +12,23 @@ const submit = document.getElementById("submit");
 submit.onclick = function() {
 
     // Gets values from forms and stores it in variables
-    let score1 = parseFloat(document.getElementById("Q1").value);
-    let score2 = parseFloat(document.getElementById("Q2").value);
-    let score3 = parseFloat(document.getElementById("Q3").value);
-    let score4 = parseFloat(document.getElementById("Q4").value);
-    let score5 = parseFloat(document.getElementById("Q5").value);
+let score1 = document.getElementById("Q1").value;
+let score2 = document.getElementById("Q2").value;
+let score3 = document.getElementById("Q3").value;
+let score4 = document.getElementById("Q4").value;
+let score5 = document.getElementById("Q5").value;
 
-    // calculates average
-    let average = (score1 + score2 + score3 + score4 + score5);
+// calculates average
+let sum = parseFloat(score1) + parseFloat(score2) + parseFloat(score3) + parseFloat(score4) + parseFloat(score5);
+let average = sum / 5;
 
-    // Delete later probably, sends average to HTML
-    document.getElementById("average").innerHTML = average.value;
+// Delete later probably, sends average to HTML
+document.getElementById("score1").innerHTML = score1;
+document.getElementById("score2").innerHTML = score2;
+document.getElementById("score3").innerHTML = score3;
+document.getElementById("score4").innerHTML = score4;
+document.getElementById("score5").innerHTML = score5;
+document.getElementById("sum").innerHTML = sum;
+document.getElementById("average").innerHTML = average;
     
 }
-
