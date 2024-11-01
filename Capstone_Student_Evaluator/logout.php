a5c1c6899ec0f922b764f1f8232a632227700614
pipno20 <lw3662wp@go.minnstate.edu>
2024-11-01 12:37:18 -0500

    Add files via upload

---
+++ Capstone_Student_Evaluator/logout.php
@@ -0,0 +1,37 @@
+<?php
+session_start(); // Start the session
+
+// Unset all session variables
+$_SESSION = [];
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page
+header("Location: login.php");
+exit;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Logout</title>
+	<link rel = "stylesheet" href="navigation.css">
+	<link rel = "stylesheet" href="signup.css">
+</head>
+
+<body>
+
+<div class="topnav">
+  <a href="index.php">Peer Review Form</a>
+  <a href="signup.php">Register</a>
+  <a href="faculty.php">Faculty</a>
+  <a href="student.php">Student</a>
+  <a href="login.php">Login</a>
+  <a href="logout.php">Logout</a>
+</div>
+</header>
+
+<body>
+</body>
+</html>
