9c76800bf118cc74a405449c19366cf04ec9a721
jova326 <113640799+jova326@users.noreply.github.com>
2024-11-10 13:04:19 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,121 @@
+This is the README file for the Capstone Student Evaluator application
+
+How to utilize PHP and MySQL using XAMPP for database development/testing
+for the Student Evaluator app:
+
+1. Ensure XAMPP installation location. 
+	- Move capstone project files to C:\xampp\htdocs\Capstone_Student_Evaluator
+	(If you installed XAMPP to default C drive in Windows.)
+2. Start XAMPP in order for testing/development
+	- Start program in Windows. Select 'Start' for both Apache and MySQL modules.
+	(Both must be running for PHP and Database functionality in Web App)
+3. Create MySQL database and Users Table (This step is only necessary once)
+	- In web browser, go to http://localhost/phpmyadmin (ensure XAMPP is running)
+	- Click 'Databases' in menu
+	- Create a new database named 'peer_review_db'
+	- Click on 'SQL' tab in menu, run the following code:
+
+		CREATE TABLE users ( 
+			id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+			username VARCHAR(50) NOT NULL UNIQUE, 
+			password VARCHAR(255) NOT NULL, 
+			role ENUM('student', 'instructor') NOT NULL, 
+			time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+		); 
+	
+	- The above code will create a users table in the peer_review_db database, 
+	you should now see the table created in the phpmyadmin page.
+	- 'id' auto increments a unique id for each user
+	- 'username' ensures a unique username must be created
+	- 'password' stores hashed password
+	- 'role ENUM' creates both necessary roles
+	- 'time_created' records account creation time.
+
+4. 'db_connection.php' can be utilized in necessary php files within the app so 
+we can simply link each page to this file instead of rewriting a connection script each time.
+Example: if you are developing the apps registration page (registration.php) its code must look like:
+<?php
+include 'db_connection.php';
+
+(All necessary code goes after include statement)
+
+?>
+
+5. Testing code in all web app files from now on should be done through localhost within the web browser
+with XAMPP running (especially for PHP functionality testing,) 
+	For example: to use the test_db_connection.php file
+	you would type http://localhost/Capstone_Student_Evaluator/test_db_connection.php 
+	in your web browser. All other files should work this way as well.
+
+
+
+
+
+***Edits to the DB to add in first_name and last_name for use on the signup page.
+
+1. Go to the localhost/phpMyAdmin page
+
+2. Go to the users DB and click on SQL tab at top of navigation bar
+
+3. Erase the lines it wants you to type and write this code to add these columns to the table.
+	ALTER TABLE users
+	ADD COLUMN first_name VARCHAR(50) NOT NULL,
+	ADD COLUMN last_name VARCHAR(50) NOT NULL;
+
+4. Press go
+
+5. That should add the new columns first_name and last_name to the DB to be able to identify users by their first and last name and not just by username/ID. You now should also be able to add entries using the signup.php page to start adding students and teachers to the database
+
+***Edits for including submission function***
+
+1. Go to localhost/phpmyadmin
+
+2. Click on SQL tab in page
+
+3. When the blank query textbox pops up, paste this:
+
+	USE peer_review_db;
+
+	CREATE TABLE submissions ( 
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	review_id INT NOT NULL, 
+	student_id INT NOT NULL, 
+	q1_rating INT, 
+	q1_comment TEXT, 
+	q2_rating INT, 
+	q2_comment TEXT, 
+	q3_rating INT, 
+	q3_comment TEXT, 
+	q4_rating INT, 
+	q4_comment TEXT, 
+	q5_rating INT, 
+	q5_comment TEXT, 
+	FOREIGN KEY (review_id) REFERENCES users(id), 
+	FOREIGN KEY (student_id) REFERENCES users(id) 
+	); 
+4. Press Go
+
+5. Now with this and the updated files from the GitHub. Reviews are able to be submitted by any logged in students, and passwords are now hashed when signing up. NOTE: you cannot use any old created users that don't have hashed passwords after the changes I've made. For testing, you will need make a new user. It will not let you log in with users that don't have a hashed password -HV
+
+**Edits for pdf generation 
+
+1. Go to this website: http://www.fpdf.org This will have you download the fpdf library that allows us to generate pdfs easier.
+
+2.Go to Downloads
+
+3. Download the first zip file that is in English. This should be the version v1.86 (2023-06-25).
+
+4. Unzip the file and make sure to put it into the same folder as the Capstone_Student_Evaluator, this will make sure the code can find the files in the right spot.
+
+5. All of the FPDF files should be in a folder named fpdf within the Capstone_Student_Evaluator folder. 
+
+** Ensures that names are searchable for active_user.php
+
+1. Go to localhost/phpmyadmin
+
+2. Click on SQL tab in page
+
+3. When the blank query textbox pops up, paste this:
+
+"ALTER TABLE `users` ADD INDEX(`first_name`);"
+"ALTER TABLE `users` ADD INDEX("last_name`);"
---
+++ signup.php
@@ -0,0 +1,135 @@
+<?php
+session_start();
+include("db_connection.php"); 
+include 'active_user.php';
+
+// include("functions.php"); where to utilize?
+
+// Checks to see that the admin hasn't been created already
+$check_admin_stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+$check_admin_stmt->execute([1]);
+$admin_check_result = $check_admin_stmt->fetch(PDO::FETCH_ASSOC);
+
+// creates the admin account if not
+if(!$admin_check_result) {
+    $admin_first_name = 'Admin';
+    $admin_last_name = '';
+    $admin_user = 'admin';
+    $admin_password = 'Password123!'; // change this later, or add a way to change password
+    $role = 'instructor';
+    $admin_id = 1;
+    
+    $hashed_admin_pass = password_hash($admin_password, PASSWORD_DEFAULT);
+    $query = "INSERT INTO users (id, first_name, last_name, username, password, role) VALUES (?, ?, ?, ?, ?, ?)";
+                $stmt = $pdo->prepare($query); 
+                //edited this to gel with PDO and password hashing in connection/submission script -HV
+                if ($stmt->execute([$admin_id, $admin_first_name, $admin_last_name, $admin_user, $hashed_admin_pass, $role])) {
+                    header("Location: signup.php");
+                    exit();
+                } else {
+                    echo "Error: " . $stmt->error;
+                }
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // Gets data from the form
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $role = 'student';
+
+    if (!empty($first_name) && !empty($last_name) && !empty($username) && !empty($password) && !is_numeric($username)) {
+       
+        // Checks to see that the username is unique
+        $check_username_stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+        $check_username_stmt->execute([$username]);
+        $result = $check_username_stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            echo "Please choose another username. Username is already taken";
+        } else {
+            // Generate an id from numbers 10-99
+            do {
+                $id = mt_rand(10, 99);
+                $check_id_stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+                $check_id_stmt->execute([$id]);
+                $result = $check_id_stmt->fetch(PDO::FETCH_ASSOC);
+            } while ($result);
+
+            // Insert data into the database
+            //modified to include password hashing for more security.
+            //Passwords can't be viewed in database anymore so write them down
+            //if you need to for testing! :) -HV
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $query = "INSERT INTO users (id, first_name, last_name, username, password, role) VALUES (?, ?, ?, ?, ?, ?)";
+            $stmt = $pdo->prepare($query); 
+            //edited this to gel with PDO and password hashing in connection/submission script -HV
+            if ($stmt->execute([$id, $first_name, $last_name, $username, $hashed_password, $role])) {
+                header("Location: login.php");
+                exit();
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+
+            //closes statement check
+            $stmt->close();
+        }
+        //closes username check
+        $check_username_stmt->close();
+    //kills program if info isn't valid    
+    } else {
+        echo "Please enter some valid information!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+	<link rel = "stylesheet" href="navigation.css">
+	<link rel = "stylesheet" href="signup.css">
+
+
+</head>
+
+<body>
+
+<div class="topnav">
+  <a class="active" href="home_page.html">Home</a>
+  <a href="index.php">Peer Review Form</a>
+  <a href="signup.php">Register</a>
+  <a href="faculty.php">Faculty</a>
+  <a href="student.php">Student</a>
+  <a href="login.php">Login</a>
+</div>
+</header>
+
+<!-- This code has users enter info to add it to the database and has them signup to use the peer review-->
+	<div id="box">
+        
+		<form method="post">
+			<h1>Sign Up</h1>
+		
+		<label for="first_name">First Name:</label>
+	       <input id="text" type="text" name="first_name"><br><br>
+		
+		<label for="last_name">Last Name:</label>
+			<input id="text" type="text" name="last_name"><br><br>
+			
+        <label for="username">Username:</label>
+			<input id="text" type="text" name="username"><br><br>
+		
+		<label for="password">Password:</label>
+			<input id="text" type="password" name="password"><br><br>
+
+			<input id="button" type="submit" name="submit" value="Sign Up"><br><br>
+
+			<a href="login.php">Click to Log in</a><br><br>
+		</form>
+	</div>
+
+</body>
+</html>
