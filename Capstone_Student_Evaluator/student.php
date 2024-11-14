fbed2eea66d8e370c6432785c7a9e775edc8316c
pipno20 <lw3662wp@go.minnstate.edu>
2024-11-14 13:25:41 -0600

    Update student.php

--- Capstone_Student_Evaluator/student.php
+++ Capstone_Student_Evaluator/student.php
@@ -43,7 +43,7 @@ if (!isset($_SESSION['id']) || $_SESSION['role'] !== 'student') {
 <header>
 <div class="topnav">
   <a href="index.php">Peer Review Form</a>
-  <a href="register.php">Register</a>
+  <a href="signup.php">Register</a>
   <a href="faculty.php">Faculty</a>
   <a href="student.php">Student</a>
   <a href="login.php">Login</a>
